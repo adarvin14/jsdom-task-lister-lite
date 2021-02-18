84f44fd78cc4cce0c79aba969216b0a0b52664f8
adarvin14 <amdarvin14@gmail.com>
2021-02-18 14:06:49 -0500

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  addEventListeners()
 });
+
+function addEventListeners() {
+  const taskSubmit = document.getElementById("create-task-form")
+  taskSubmit.addEventListener('submit', handleSubmit)
+}
+
+function handleSubmit(e) {
+  e.preventDefault()
+  const input = e.target.querySelector("#new-task-description")
+  const inputValue = input.value
+  const li = document.createElement('li')
+  li.textContent = inputValue
+  const task = document.querySelector("#tasks")
+  task.appendChild(li)
+  e.target.querySelector("#new-task-description").value = ""
+}
